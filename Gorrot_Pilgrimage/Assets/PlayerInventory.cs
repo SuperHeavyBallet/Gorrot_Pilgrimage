3ad7061a2b750d987d307d4b3d17b768c73326a7
SuperHeavyBallet <agram@hotmail.co.uk>
2026-01-17 23:45:47 +0100

    Refactor Check AddItem

--- Gorrot_Pilgrimage/Assets/PlayerInventory.cs
+++ Gorrot_Pilgrimage/Assets/PlayerInventory.cs
@@ -8,11 +8,12 @@ public class PlayerInventory : MonoBehaviour
 
     public GameObject[] inventorySlots = new GameObject[4];
 
-    bool hasFreeSlot = true;
-
     List<InventoryItemTemplate> allItemsList = new List<InventoryItemTemplate>();
 
     public ItemCatalogue itemCatalogue;
+
+    private const int SlotNotFound = -1;
+    private const int DuplicateItem = -2;
 
     public void BubbleSortEmptySlotsToEnd()
     {
@@ -49,105 +50,59 @@ public class PlayerInventory : MonoBehaviour
         
     }
 
-    InventorySlotController GetInventorySlotController(int index)
-    {
-        return inventorySlots[index].GetComponent<InventorySlotController>();
-
-    }
-
-    void AddDuplicateItems(string itemID, int slotIndex)
-    {
-        Debug.Log("Should Add Duplicate in slot: " + slotIndex);
-    }
-
     public bool TryToAddItem(string itemID)
     {
 
+        var slotResult = CheckFreeSlot(itemID);
 
-        // Here Point of Entry
-        // Find a Free Slot, the items slots *should* be sorted before this (after previous update loop), but maybe pre-sort them to make sure too
+        if (slotResult.SlotController == null) return false;
 
-        int freeSlotIndex = FindFreeSlot(itemID);
-
-        // First, check if the Inventory already has this item:
-
-
-        /*
-
-       for (int i = 0; i < inventorySlots.Length; i++)
+        if (slotResult.SlotIndex == DuplicateItem)
         {
-            InventorySlotController inventorySlotController = GetInventorySlotController(i);
-
-            Debug.Log("Current item in slot: " + i + " : " + inventorySlotController.GetCurrentItemID());
-
-            if(inventorySlotController.GetCurrentItemID() == itemID)
-            {
-
-                Debug.Log("Item Already Held");
-                break;
-            }
-
-
-        }*/
-
-        // -2 Is the 'Add Duplicate' State - maybe find more elegant fix
-
-        if (freeSlotIndex != -1 && freeSlotIndex != -2 && hasFreeSlot)
-        {
-            AddNewItem(itemID, freeSlotIndex);
+            if (slotResult.SlotController == null) return false;
+            slotResult.SlotController.PlaceDuplcateItemInSlot();
             return true;
         }
-        else if(freeSlotIndex == -2)
+        else if(slotResult.SlotIndex >= 0)
         {
+            AddNewItem(itemID, slotResult.SlotIndex);
             return true;
         }
-        else
-        {
-            return false;
-        }
-
-
+    
+        return false;
 
     }
 
-
-    int FindFreeSlot(string newItemID)
+    struct SlotSearchResult
     {
+        public int SlotIndex;
+        public InventorySlotController SlotController;
+    }
 
-        hasFreeSlot = false;
 
-        int freeSlotIndex = -1;
+    SlotSearchResult CheckFreeSlot(string newItemID)
+    {
 
         for (int i = 0;i < inventorySlots.Length;i++)
         {
             InventorySlotController inventorySlotController = inventorySlots[i].GetComponent<InventorySlotController>();
-
             string heldItemID = inventorySlotController.GetCurrentItemID();
 
-            Debug.Log("ITEM ID: " + newItemID + ", IN SLOT OF " + heldItemID);
-
-            // IF the existing itemID (Including Empty) does not match the new itemID, e.g. 'potion' vs 'empty' > Check if that slot IS Empty, if so, break the loop and return that slot index
             if(heldItemID != newItemID)
             {
                 if (inventorySlotController.CheckSlotEmpty())
                 {
-                    hasFreeSlot = true;
-                    freeSlotIndex = i;
-                    break;
+                    return new SlotSearchResult { SlotIndex = i, SlotController = inventorySlotController };
                 }
             }
-            // Else, if the existing itemID DOES match the new itemID > it is already held > Add a duplicate (increment quantity) and return -2 instead of an actual slot index
             else
             {
-                inventorySlotController.PlaceDuplcateItemInSlot();
-                freeSlotIndex = -2;
-                break;
+                return new SlotSearchResult { SlotIndex = DuplicateItem, SlotController = inventorySlotController };
+               
             }
-           
-
         }
 
-        return freeSlotIndex;
+        return new SlotSearchResult { SlotIndex = SlotNotFound, SlotController = null };
 
 
     }
